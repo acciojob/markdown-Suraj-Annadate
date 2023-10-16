0b0259682dfe6d4419c4103aa0ee208df3db0753
suraj0698 <annadatesuraj@gmail.com>
2023-10-16 19:16:04 +0530

    c2

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React ,{useState} from 'react';
 import '../styles/App.css'
 
-const App = () => {
-   const[markdown,setMarkdown] = useState("");
 
-   return(<div className='app'>
-    <textarea value={markdown} onChange={(e)=> setMarkdown(e.target.value)}/>
-    <div className='preview'>
-    <h1>{markdown}</h1>
-    </div>
-   </div>)
-}
+const App=()=>{
+    let [val,setVal]=useState('');
+  
+    function changetext(event){
+        setVal(event.target.value)
+    }
+    return (
+       <div className="app">
+         <div className='mark1'>
+            <div className='preview'>
+            <h1>Heading</h1><br></br><br></br><span>This is some <strong>bold</strong> text.</span>  
+             <textarea onChange={changetext}>
+            </textarea>   
+            </div>
+        </div>
+       </div>
+    )
+}   
 
-export default App
+export default App;
